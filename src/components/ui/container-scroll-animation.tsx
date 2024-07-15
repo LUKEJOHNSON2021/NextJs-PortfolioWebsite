0351db7ec5e232bcbefb3a1817fe83b8a432329a
LUKEJOHNSON2021 <LUKE.JOHNSON-2021@hull.ac.uk>
2024-07-15 14:11:16 +0100

    Update container-scroll-animation.tsx
    update start pos

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -26,7 +26,7 @@ export const ContainerScroll = ({ titleComponent, children }: { titleComponent: 
 
   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const translate = useTransform(scrollYProgress, [0, 1], [-20, -100]);
 
   return (
     <div className="h-[60rem] md:h-[80rem] flex items-center justify-center relative p-2 md:p-20" ref={containerRef}>
