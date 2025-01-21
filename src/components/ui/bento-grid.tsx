664fca42a03d50f2c914a321a6dc56547da597cf
Luke <lukej115@hotmail.com>
2025-01-21 10:36:17 +0000

    change to bento-grid

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -29,12 +29,12 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-zinc-900 bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-zinc-900 bg-white border border-transparent justify-between flex flex-col space-y-4 overflow:hidden",
         className
       )}
       onClick={() => navigate(url)}
     >
-      {header}
+      <div className="overflow-hidden rounded-lg">{header}</div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">{title}</div>
